ccb820cb02c7bee7203fb24b6fb100b930afff59
Mamun <beparimamun708@gmail.com>
2026-05-07 15:47:00 +0600

    Added print.py

---
+++ Introduction/print.py
@@ -0,0 +1,15 @@
+
+# Python is a high-level programming language known for its simple and radable syntax. 
+# It has the following features:
+#    1. Allows writing programs with fewer lines of code, improving readability.
+#    2. Automatically detects variable types at runtime, eliminating the need for explicit declarations. 
+#    3. Used in web development, data analysis, automation and many other fields .
+#    4. Supports object-oriented, functional and procedural programming styles.
+#    5. Dynamically typed and has automatic garbage collection.
+
+print("Hello, World!")
+# "Hello World" is printed on screen 
+# print() function prints messsage on screeen.
+# print() is a built-in Python function that instructs the computer to display text on the screen.
+# "Hello, World!" is a string, which is sequence of text. Strings are enclosed in quotes (either single ' or double ").
+# The scope of code blocks is dedined using Indentation (spaces or tabs). This is different from C, C++ and Java which use braces {} to create a block of code.
