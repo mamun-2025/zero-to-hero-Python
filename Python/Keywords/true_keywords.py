d3ee1dfef9c002aaba0aba1330fa03aef8139a17
Mamun <beparimamun708@gmail.com>
2026-05-16 00:28:35 +0600

    Added True Keyword

---
+++ Python/Keywords/true_keywords.py
@@ -0,0 +1,24 @@
+
+if True:
+   print("This will always execute")
+
+# 1. Using True in Conditional Statements
+x = 10
+f = x > 5
+
+if f:
+   print("X is greater than 5")
+
+
+# 2. Using True in Loops
+c = 0
+while True:
+   print(c)
+   c += 1
+   if c == 5:
+      break # Stops the loop when count reaches 5
+
+
+# 3. Using True as 1 in Arithmetic Operations
+print(True + 5)
+print(True * 5)
