4f3b29257329d64a16f47bb8e99600dc8f36e05d
Mamun <beparimamun708@gmail.com>
2026-05-17 16:44:37 +0600

    Added Assignment Operator

---
+++ Python/Operators/assignment_operators.py
@@ -0,0 +1,66 @@
+
+# Assignment Operators in Python
+"""
+The python operators are used to perform operations on values and varables.
+These are the special symbols that carry out arithmetic, and bitwise computations.
+The value the operator operates on is known as the Operand.
+Assignement Operators are used to assign values to variables.
+This operator is used to assign the value of the right side of the expression to the left side operand.
+"""
+# Assigning values using 
+# Assignment Operator
+a = 3
+b = 5
+
+c = a + b 
+
+print(c)
+
+# 1. Addition Assignment Operator
+a = 3 
+b = 5
+
+a += b # a = a + b 
+print(a)
+
+# 2. Subtraction Assignment Operator
+a = 3 
+b = 5
+
+a -= b # a = a - b
+print(a)
+
+# 3. Multiplication Assignment Operator 
+a = 3 
+b = 5
+
+a *= b # a = a * b
+print(a)
+
+# 4. Division Assignment Operator
+a = 3 
+b = 5
+
+a /= b # a = a / b
+print(a)
+
+# 5. Modulus Assignment Operator
+a = 3
+b = 5
+
+a %= b # a = a % b
+print(a)
+
+# 6. Floor Division Assignment Operator
+a = 3
+b = 5
+
+a //= b # a = a // b
+print(a)
+
+# 7. Exponentiation Assignment Operator
+a = 3 
+b = 5
+
+a **= b # a = a ** b
+print(a)
