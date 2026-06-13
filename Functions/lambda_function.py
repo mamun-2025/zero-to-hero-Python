94e360e36124dd2815830af54a74b524352c06fe
Mamun <beparimamun708@gmail.com>
2026-06-13 13:32:39 +0600

    Added lambda_function.py

--- Functions/lambda_function.py
+++ Functions/lambda_function.py
@@ -8,7 +8,8 @@ These are small, short-lived functions used to pass simple logic to another func
 
 1. Lambda Function কী?
 Lambda Function Python-এর একটি ছোট anonymous function (নামবিহীন ফাংশন)।
-Lambda Function হলো Python-এর anonymous function যা lambda arguments: expression syntax ব্যবহার করে এক লাইনে ছোট function তৈরি করতে সাহায্য করে। 
+Lambda Function হলো Python-এর anonymous function 
+যা lambda arguments: expression syntax ব্যবহার করে এক লাইনে ছোট function তৈরি করতে সাহায্য করে। 
 এটি সাধারণত map(), filter(), sorted() ইত্যাদির সাথে ব্যবহৃত হয়।
 
 2. Lambda-এর Limitations
@@ -25,46 +26,46 @@ lambda x:
 ❌ Multiple statement লেখা যায় না।
 
 """
-# # simple function
-# def add(a, b):
-#    return a + b 
+# simple function
+def add(a, b):
+   return a + b 
 
-# print(add(10, 20))
+print(add(10, 20))
 
-# # Lambda version:
-# add = lambda a, b: a + b 
-# print(add(10, 20))
+# Lambda version:
+add = lambda a, b: a + b 
+print(add(10, 20))
 
-# # Lambda Syntax: lambda arguments : expression
-# # example:
-# square = lambda x: x * x 
-# print(square(5))
+# Lambda Syntax: lambda arguments : expression
+# example:
+square = lambda x: x * x 
+print(square(5))
 
-# # same thing
-# def square(x):
-#    return x * x 
+# same thing
+def square(x):
+   return x * x 
 
-# print(square(5))
+print(square(5))
 
-# ######## Use cases
-# # 1. Condition checking:
-# check = lambda x: "Positive" if x > 0 else "Negative" if x < 0 else "Zero"
+######## Use cases
+# 1. Condition checking:
+check = lambda x: "Positive" if x > 0 else "Negative" if x < 0 else "Zero"
 
-# print(check(5))   
-# print(check(-3))  
-# print(check(0))
-
-
-# # 2. List Comprehension
-# func = [lambda arg=x: arg * 10 for x in range(1, 5)]
-# for i in func:
-#    print(i())
+print(check(5))   
+print(check(-3))  
+print(check(0))
 
 
-# # 3. Returning Multiple Results
-# calc = lambda x, y: (x + y , x * y)
-# res = calc(2, 4)
-# print(res)
+# 2. List Comprehension
+func = [lambda arg=x: arg * 10 for x in range(1, 5)]
+for i in func:
+   print(i())
+
+
+# 3. Returning Multiple Results
+calc = lambda x, y: (x + y , x * y)
+res = calc(2, 4)
+print(res)
 
 
 # 4. filter()
@@ -123,292 +124,292 @@ active_users = list(
 print(active_users)
 
 
-# # 5. map()
-# ##
-# s = ['1', '2', '3', '4']
-# res = list(map(int, s))
-# print(res)
+# 5. map()
+##
+s = ['1', '2', '3', '4']
+res = list(map(int, s))
+print(res)
 
-# ##
-# def double(val):
-#    return val * 2
+##
+def double(val):
+   return val * 2
 
-# a = [1, 2, 3, 4]
-# res = list(map(double, a))
-# print(res)
+a = [1, 2, 3, 4]
+res = list(map(double, a))
+print(res)
 
-# ##
-# a = [1, 2, 3, 4]
-# double = map(lambda x: x * 2, a)
-# print(list(double))
+##
+a = [1, 2, 3, 4]
+double = map(lambda x: x * 2, a)
+print(list(double))
 
-# ##
-# a = [1, 2, 3, 4]
-# res = list(map(lambda x: x ** 2, a))
-# print(res)
-# ##
-# numbers = [1, 2, 3, 4, 5]
-# result = list(map(lambda x: x*x, numbers))
-# print(result)
+##
+a = [1, 2, 3, 4]
+res = list(map(lambda x: x ** 2, a))
+print(res)
+##
+numbers = [1, 2, 3, 4, 5]
+result = list(map(lambda x: x*x, numbers))
+print(result)
 
-# ## 
-# salaries = [10000, 20000, 30000]
-# new_salaries = list(
-#    map(lambda salary: salary * 10.0, salaries
-# ))
-# print(new_salaries)
+## 
+salaries = [10000, 20000, 30000]
+new_salaries = list(
+   map(lambda salary: salary * 10.0, salaries
+))
+print(new_salaries)
 
-# ##
-# a = [1, 2, 3]
-# b = [4, 5, 6]
-# res = list(map(lambda x, y: x + y, a, b))
-# print(res)
+##
+a = [1, 2, 3]
+b = [4, 5, 6]
+res = list(map(lambda x, y: x + y, a, b))
+print(res)
 
-# ##
-# fruits = ["apple", "banana", "cherry"]
-# res = list(map(str.upper, fruits))
-# print(res)
+##
+fruits = ["apple", "banana", "cherry"]
+res = list(map(str.upper, fruits))
+print(res)
 
-# ##
-# words = ["apple", "banana", "cherry"]
-# res = list(map(lambda s: s[1], words))
-# print(res)
+##
+words = ["apple", "banana", "cherry"]
+res = list(map(lambda s: s[1], words))
+print(res)
 
-# ## 
-# s = [" hello ", " world ", " python "]
-# res = list(map(str.strip, s))
-# print(res)
+## 
+s = [" hello ", " world ", " python "]
+res = list(map(str.strip, s))
+print(res)
 
-# ## 
-# celsius = [0, 20, 37, 100]
-# fahrenheit = list(map(lambda c: (c * 9/5) + 32, celsius))
-# print(fahrenheit)
-
-
-
-# # 6. reduce()
-# from functools import reduce
-# a = [1, 2, 3, 4]
-# multiply = reduce(lambda x, y: x * y, a)
-# print(multiply)
+## 
+celsius = [0, 20, 37, 100]
+fahrenheit = list(map(lambda c: (c * 9/5) + 32, celsius))
+print(fahrenheit)
 
 
-# # 7. sorted()
-# students = [
-#    {"name": "Mamun", "age": 25},
-#    {"name": "Habib", "age": 30},
-#    {"name": "Karim", "age": 28}
-# ]
-# result = sorted(students, key=lambda student: student["age"])
-# print(result)
+
+# 6. reduce()
+from functools import reduce
+a = [1, 2, 3, 4]
+multiply = reduce(lambda x, y: x * y, a)
+print(multiply)
 
 
-# # 8. min()
-# employees = [
-#    {"name": "mamun", "salary": 50000},
-#    {"name": "habib", "salary": 70000},
-#    {"name": "Rudro", "salary": 20000}
-# ]
-
-# lowest_salary = min(
-#    employees,
-#    key=lambda emp: emp["salary"]
-# )
-
-# print(lowest_salary)
+# 7. sorted()
+students = [
+   {"name": "Mamun", "age": 25},
+   {"name": "Habib", "age": 30},
+   {"name": "Karim", "age": 28}
+]
+result = sorted(students, key=lambda student: student["age"])
+print(result)
 
 
-# # 9. max()
-# ##
-# employees = [
-#     {"name": "A", "salary": 50000},
-#     {"name": "B", "salary": 20000},
-#     {"name": "C", "salary": 70000}
-# ]
+# 8. min()
+employees = [
+   {"name": "mamun", "salary": 50000},
+   {"name": "habib", "salary": 70000},
+   {"name": "Rudro", "salary": 20000}
+]
 
-# highest_salary = max(
-#     employees,
-#     key=lambda emp: emp["salary"]
-# )
+lowest_salary = min(
+   employees,
+   key=lambda emp: emp["salary"]
+)
 
-# print(highest_salary)
+print(lowest_salary)
 
-# ## 
-# users = [
-#     {"name": "Mamun", "age": 25},
-#     {"name": "Rahim", "age": 18},
-#     {"name": "Karim", "age": 30}
-# ]
 
-# oldest = max(
-#     users,
-#     key=lambda user: user["age"]
-# )
+# 9. max()
+##
+employees = [
+    {"name": "A", "salary": 50000},
+    {"name": "B", "salary": 20000},
+    {"name": "C", "salary": 70000}
+]
 
-# print(oldest)
+highest_salary = max(
+    employees,
+    key=lambda emp: emp["salary"]
+)
+
+print(highest_salary)
+
+## 
+users = [
+    {"name": "Mamun", "age": 25},
+    {"name": "Rahim", "age": 18},
+    {"name": "Karim", "age": 30}
+]
+
+oldest = max(
+    users,
+    key=lambda user: user["age"]
+)
+
+print(oldest)
 
 #######################################################################
 
-# # Example 1: দুই সংখ্যার যোগ
-# add = lambda a, b: a + b 
-# print(add(10, 10))
+# Example 1: দুই সংখ্যার যোগ
+add = lambda a, b: a + b 
+print(add(10, 10))
 
-# # Example 2: তিনটি সংখ্যার যোগ
-# add = lambda a, b, c: a + b + c
-# print(add(10, 10, 10))
+# Example 2: তিনটি সংখ্যার যোগ
+add = lambda a, b, c: a + b + c
+print(add(10, 10, 10))
 
-# # Example 3: Even/Odd Check
-# is_even = lambda x: x % 2 == 0
-# print(is_even(8))
-# print(is_even(7))
+# Example 3: Even/Odd Check
+is_even = lambda x: x % 2 == 0
+print(is_even(8))
+print(is_even(7))
 
-# # Example 4: বড় সংখ্যা বের করা
-# largest = lambda a, b: a if a > b else b 
-# print(largest(10, 3))
+# Example 4: বড় সংখ্যা বের করা
+largest = lambda a, b: a if a > b else b 
+print(largest(10, 3))
 
-# # function 
-# def largest(a, b):
-#    if a > b:
-#       return a 
-#    else:
-#       return b 
+# function 
+def largest(a, b):
+   if a > b:
+      return a 
+   else:
+      return b 
    
-# print(largest(10, 5))
+print(largest(10, 5))
 
-# # Example 5: String Uppercase 
-# upper = lambda name: name.upper()
-# print(upper("mamun"))
+# Example 5: String Uppercase 
+upper = lambda name: name.upper()
+print(upper("mamun"))
 
-# # Example 6: String Lowercase
-# lower = lambda name: name.lower()
-# print(lower("NONDITA"))
+# Example 6: String Lowercase
+lower = lambda name: name.lower()
+print(lower("NONDITA"))
 
-# # Example 7: String Length
-# length = lambda text: len(text)
-# print(length("GeeksforGeeks"))
+# Example 7: String Length
+length = lambda text: len(text)
+print(length("GeeksforGeeks"))
 
-# # Exampole 8: List এর সব সংখ্যা Double করা
-# numbers = [1, 2, 3, 4, 5]
-# result = list(map(lambda x: x*2, numbers))
-# print(result)
+# Exampole 8: List এর সব সংখ্যা Double করা
+numbers = [1, 2, 3, 4, 5]
+result = list(map(lambda x: x*2, numbers))
+print(result)
 
-# # Example 9: সব নাম Capital Letter করা
-# names = ["mamun", "nondita", "sanjib"]
-# result = list(map(lambda name: name.upper(), names))
-# print(result)
+# Example 9: সব নাম Capital Letter করা
+names = ["mamun", "nondita", "sanjib"]
+result = list(map(lambda name: name.upper(), names))
+print(result)
 
-# # Example 10: শুধু Positive Number বের করা
-# numbers = [1, -3, 4, -8, -5, 7, 9]
-# positive_number = list(filter(lambda x: x > 0, numbers))
-# print(positive_number)
+# Example 10: শুধু Positive Number বের করা
+numbers = [1, -3, 4, -8, -5, 7, 9]
+positive_number = list(filter(lambda x: x > 0, numbers))
+print(positive_number)
 
-# # Example 11: শুধু Odd Number
-# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-# even_numbers = list(filter(lambda x: x % 2 == 0, numbers ))
-# print(even_numbers)
-# odd_numbers = list(filter(lambda x: x % 2 != 0, numbers))
-# print(odd_numbers)
+# Example 11: শুধু Odd Number
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+even_numbers = list(filter(lambda x: x % 2 == 0, numbers ))
+print(even_numbers)
+odd_numbers = list(filter(lambda x: x % 2 != 0, numbers))
+print(odd_numbers)
 
-# # Example 12: নামের Length অনুযায়ী Sort
-# names = ["Mamun", "A", "Nondita", "Kobi"]
-# sorted_names = sorted(names, key=lambda x: len(x))
-# print(sorted_names)
+# Example 12: নামের Length অনুযায়ী Sort
+names = ["Mamun", "A", "Nondita", "Kobi"]
+sorted_names = sorted(names, key=lambda x: len(x))
+print(sorted_names)
 
-# # Example 13: Employee Salary Sort
-# employees = [
-#    {"name": "Mamun", "salary": 50000},
-#    {"name": "Rahim", "salary": 70000},
-#    {"name": "Karim", "salary": 60000}
-# ]
+# Example 13: Employee Salary Sort
+employees = [
+   {"name": "Mamun", "salary": 50000},
+   {"name": "Rahim", "salary": 70000},
+   {"name": "Karim", "salary": 60000}
+]
 
-# result = sorted(
-#    employees, key=lambda x: x["salary"]
-# )
-# print(result)
+result = sorted(
+   employees, key=lambda x: x["salary"]
+)
+print(result)
 
-# # Example 14: Django Backend Type Example 
-# users = [
-#    {"username": "mamun", "age": 15},
-#    {"username": "habib", "age": 30},
-#    {"username": "karim", "age": 18}
-# ]
+# Example 14: Django Backend Type Example 
+users = [
+   {"username": "mamun", "age": 15},
+   {"username": "habib", "age": 30},
+   {"username": "karim", "age": 18}
+]
 
-# adults = list(
-#    filter(
-#       lambda user: user["age"] >= 18 ,
-#       users
-#    )
-# )
+adults = list(
+   filter(
+      lambda user: user["age"] >= 18 ,
+      users
+   )
+)
 
-# print(adults)
+print(adults)
 
-# # Example 15: Lambda Returning Lambda
-# def mutiplier(n):
-#    return lambda x: x * n 
+# Example 15: Lambda Returning Lambda
+def mutiplier(n):
+   return lambda x: x * n 
 
-# double = mutiplier(2)
-# triple = mutiplier(3)
+double = mutiplier(2)
+triple = mutiplier(3)
 
-# print(double(5))
-# print(triple(5))
+print(double(5))
+print(triple(5))
 
-# # Example 16: Multiple Condition
-# check = lambda x: "Even" if x % 2 == 0 else "Odd"
+# Example 16: Multiple Condition
+check = lambda x: "Even" if x % 2 == 0 else "Odd"
 
-# print(f"This number is:", check(10))
-# print(f"This number is:", check(7))
+print(f"This number is:", check(10))
+print(f"This number is:", check(7))
 
-# # Example 17: Backend Interview Level Example
-# orders = [
-#    {"id": 1, "amount": 5000},
-#    {"id": 2, "amount": 3000},
-#    {"id": 3, "amount": 9000}
-# ]
+# Example 17: Backend Interview Level Example
+orders = [
+   {"id": 1, "amount": 5000},
+   {"id": 2, "amount": 3000},
+   {"id": 3, "amount": 9000}
+]
 
-# highest_orders = max(
-#    orders, 
-#    key=lambda order: order["amount"]
-# )
-# print(highest_orders)
+highest_orders = max(
+   orders, 
+   key=lambda order: order["amount"]
+)
+print(highest_orders)
 
 
 # #################################################################
-# ## Real Backend Project Exmple
-# orders = [
-#    {"id": 1, "amount": 500},
-#    {"id": 2, "amount": 700},
-#    {"id": 3, "amount": 400}
-# ]
+## Real Backend Project Exmple
+orders = [
+   {"id": 1, "amount": 500},
+   {"id": 2, "amount": 700},
+   {"id": 3, "amount": 400}
+]
 
-# # সবচেয়ে বড় Order
-# max_order = max(
-#    orders, 
-#    key=lambda order: order["amount"]
-# )
-# print(max_order)
+# সবচেয়ে বড় Order
+max_order = max(
+   orders, 
+   key=lambda order: order["amount"]
+)
+print(max_order)
 
-# # সবচেয়ে ছোট Order
-# min_order = min(
-#    orders,
-#    key=lambda order: order["amount"]
-# )
-# print(min_order)
+# সবচেয়ে ছোট Order
+min_order = min(
+   orders,
+   key=lambda order: order["amount"]
+)
+print(min_order)
 
-# # Amount অনুযায়ী Sort
-# sorted_orders = sorted(
-#    orders,
-#    key=lambda order: order["amount"]
-# )
-# print(sorted_orders)
+# Amount অনুযায়ী Sort
+sorted_orders = sorted(
+   orders,
+   key=lambda order: order["amount"]
+)
+print(sorted_orders)
 
-# # 500 টাকার বেশি Order
-# big_orders = list(
-#    filter(
-#       lambda order: order["amount"] > 500,
-#       orders
-#    )
-# )
-# print(big_orders)
+# 500 টাকার বেশি Order
+big_orders = list(
+   filter(
+      lambda order: order["amount"] > 500,
+      orders
+   )
+)
+print(big_orders)
 
 """
 map() = ➡️ Transform / পরিবর্তন করে
