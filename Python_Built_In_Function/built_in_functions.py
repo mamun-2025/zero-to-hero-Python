f9004e232821c43f45d38b381b839183b5fd33ab
Mamun <beparimamun708@gmail.com>
2026-06-24 23:34:00 +0600

    Added built-in-functions

--- Python_Built_In_Function/built_in_functions.py
+++ Python_Built_In_Function/built_in_functions.py
@@ -1,2 +1,19 @@
 
 
+##### Group 1: Object and Type Checking
+## len()
+print(len("Python"))
+
+fruits = ["apple", "mango", "cherry"]
+print(len(fruits))
+
+data = {"name": "mamun",
+        "age": 25
+}
+print(len(data))
+
+
+numbers = [1, 2, 3, 4]
+print(len(numbers))
+
+
