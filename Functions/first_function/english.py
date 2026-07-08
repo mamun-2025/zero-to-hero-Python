ae2dfc1931250c9492fc4db35bdf2013f1fe5735
Mamun <beparimamun708@gmail.com>
2026-07-08 23:01:58 +0600

    Added english.py

--- Functions/first_function/english.py
+++ Functions/first_function/english.py
@@ -1,1 +1,122 @@
 
+
+##### Python Functions - Teaching Notes(English Practice)
+"""
+1.What is a Function?
+= A function is a reusable block of code that performs a specific task.
+  Instead of writing the same code multiple times, we can create a function once and call it whenever we need it.
+  Function help us:
+  - reduce code repetition
+  - organize our program
+  - improve code readability
+  - make code easier to maintain
+= A function is like a small machine. We give some input to the function, it performs a specific operation, and it can return an output.
+
+Vacabulary:
+reusable = can be used again
+block = a group/section
+specific = particular
+task = work/job
+organize = arrange properly
+maintain = keep working properly
+readability = easy to understand
+repetition = doing something again
+
+Sentence:
+1. A function is a reusable block of code.
+2. A function performs a specific task.
+3. We create functions to avoid repeating code.
+4. Functions make programs easier to understand.
+5. We can call a function whenever we need it.
+
+1. In Python, functions are used to organize code into smaller reusable components.
+2. Functions improve code maintainability and reduce duplication.
+3. A function can accept input values through parameters and return results using the return statement.
+
+
+
+
+
+2. Python def keyword
+The def keyword is used to define a function in Python.
+When we create a function, we use:
+   def function_name():
+      code
+
+   def greet():
+   print("Hello")
+
+def = creates a function
+greet = function name
+() = Parameters area
+print() = function body
+
+Explanation:
+"First, we use the def keyword to define a function named greet.
+ Inside the function body, we write the code that we want to execute. 
+ The print statement will run when we call the function."
+
+Vocabulary:
+define = create something
+keyword = reserved word in programming
+execute = run code
+function body = code inside function
+statement = instruction
+call = execute a function
+
+
+
+
+
+
+3. Calling a Function
+   def welcome():
+      print("Welcome")
+
+   welcome()
+
+"Defining a function does not execute the code.The function runs only when we call it using the function name followed by parentheses.
+ A function definition only creates the function, but a function call executes the function."
+
+Vocabulary:
+definition = creating something
+execution = running something
+invoke = call a function
+trigger = start an action 
+
+
+
+
+
+4. Parameters and Arguments 
+This is very important for interveiws.
+   def greet(name):
+      print(name)
+
+   greet("Mamun")
+
+name = parameter(name is a parameter.)
+greet("Mamun") = argument("Mamun" is an argument)
+
+Explanation:
+"A parameter is a variable that receives data when we define a function.
+ An argument is the actual value that we pass during the function call."
+
+Vocubulary:
+paremeter = variable in function definition
+argument = actual value passed to function
+receive = get something
+pass = send something
+value = data stored in variable
+
+"A function can receive data through parameters. When we call the function,
+ we provide arguments. Parameters act as placeholders, while arguments are the actual values."
+
+ 
+
+
+
+ 5. Return Statement
+ 
+
+"""
