6e33b1373c946ee246def5de780c57aabdd2821c
Mamun <beparimamun708@gmail.com>
2026-07-12 17:48:40 +0600

    Added itertool and function tool

--- Standard_Library/itertool&functools_module/functool_module.py
+++ Standard_Library/itertool&functools_module/functool_module.py
@@ -1,2 +1,453 @@
 
 
+### functools কী?
+"""
+functools হলো এমন module যেখানে function নিয়ে কাজ করার 
+কিছু powerful tools আছে।
+
+ব্যবহার:
+Higher order function
+Decorator
+Function optimization
+
+"""
+import functools
+
+
+### 1. reduce()
+# reduce কী?
+# অনেকগুলো value নিয়ে একটি final result দেয়।
+# normal 
+numbers = [1, 2, 3, 4]
+
+total = 1
+
+for n in numbers:
+   total *= n
+
+print(total)
+
+
+# reduce দিয়ে:
+from functools import reduce
+
+numbers = [1, 2, 3, 4]
+
+result = reduce(lambda x, y: x*y, numbers)
+
+print(result)
+
+##
+from functools import reduce
+prices = [
+   100,
+   200,
+   300,
+]
+
+total = reduce(
+   lambda x, y: x+y, prices
+)
+
+print(total)
+
+
+
+
+### 2. partial()
+# partial কী?
+# partial() একটি existing function থেকে নতুন function তৈরি করে 
+# যেখানে কিছু argument আগে থেকেই fixed থাকে।
+def multiply(a, b):
+   return a * b 
+
+
+from functools import partial
+
+multiply_by_10 = partial(
+   multiply,
+   10
+)
+
+print(multiply_by_10(5))
+
+# Backend Real Example: Database Connection
+def connect_database(host, port, username):
+   print(host, port, username)
+
+
+from functools import partial
+
+production_db = partial(
+   connect_database,
+   host="loacalhost",
+   port="5432"
+)
+
+production_db(username="admin ")
+
+
+##
+from functools import partial
+
+def send_email(to, smtp, port):
+   print(f"Sending email to {to} using {smtp}:{port}")
+
+
+gmail_email= partial(
+   send_email,
+   smtp="gmail",
+   port=5432
+)
+
+gmail_email("user1@email.com")
+gmail_email("user2@gmail.com")
+
+
+
+
+### 3. wraps()
+# এটা Decorator-এর জন্য খুব গুরুত্বপূর্ণ।
+# আমরা আগে decorator শিখেছি।
+def decorator(func):
+
+   def wrapper():
+
+      return func()
+   
+   return wrapper
+# Function information হারিয়ে যায়।
+# solution:
+from functools import wraps
+
+def decorator(func):
+
+   @wraps(func)
+   def wrapper():
+
+      return func()
+   
+   return wrapper
+
+# Django middleware/decorator:
+
+# Login required
+# Permission check
+# Logging
+
+# এখানে wraps ব্যবহার করা হয়।
+   
+
+
+
+
+### 4. lru_cache()
+# lru_cache কী?
+# Function result cache করে।
+# যাতে একই calculation বারবার করতে না হয়।
+from functools import lru_cache
+
+@lru_cache
+def fibonacci(n):
+
+   if n <= 1:
+      return n 
+   
+   return fibonacci(n-1)+fibonacci(n-2)
+
+print(fibonacci(10))
+
+"""
+Without cache:
+অনেক calculation হয়।
+
+With cache:
+আগের result মনে রাখে।
+
+Backend Example:
+
+Expensive calculation
+API data caching
+Repeated query optimization
+
+
+itertools vs functools
+| itertools            | functools            |
+| -------------------- | -------------------- |
+| Data processing      | Function processing  |
+| Iterator tools       | Function tools       |
+| Loop optimization    | Function reuse       |
+| combinations/product | reduce/partial/cache |
+
+"""
+
+# Problem 1: Product list থেকে সব possible pair বের করা
+from itertools import combinations
+
+products = [
+   "A",
+   "B",
+   "C"
+]
+
+pairs = combinations(
+   products,
+   2
+)
+
+for pair in pairs:
+   print(pair)
+
+
+# Problem 2: E-commerce Variation Generator
+from itertools import product
+
+colors = [
+   "Black",
+   "White"
+]
+
+sizes = [
+   "M",
+   "L"
+]
+
+variations = product(
+   colors,
+   sizes
+)
+
+for variation in variations:
+   print(variation)
+
+# Real ecommerce example:
+from itertools import product
+
+colors = [
+   "Red",
+   "Black"
+]
+
+sizes = [
+   "M",
+   "S"
+]
+
+products = []
+
+for color, size in product(colors, sizes):
+
+   products.append(
+     {
+      "color":color,
+      "size": size
+     }
+   )
+
+print(products)
+
+
+# Problem 3: Cart Price using reduce()
+from functools import reduce
+
+prices = [
+   100,
+   200,
+   500
+]
+
+total = reduce(
+   lambda x, y: x + y, prices
+)
+
+print("Cart Total:", total)
+
+
+# Problem 4: Decorator with @wraps
+# Requirement:
+# ✅ Function name দেখাবে
+# ✅ Execution message দেখাবে
+# ✅ @wraps ব্যবহার করবে
+
+from  functools import wraps
+
+def execution_logger(func):
+
+   @wraps(func)
+   def wrapper(*args, **kwargs):
+
+      print(
+         "Function started", 
+         func.__name__
+      )
+
+      result = func(
+         *args,
+         **kwargs
+      )
+
+      print(
+         "Function Finished",
+         func.__name__
+      )
+
+      return result
+   
+   return wrapper
+
+@execution_logger
+def create_order():
+   print("Order Created")
+   
+create_order()
+
+print(create_order.__name__)
+
+
+# Problem 5: Backend Mini Project
+# Order ID Generator System
+
+# Features:
+# ✅ Automatic ID generation
+# ✅ Product combination
+# ✅ Cart total calculation
+# ✅ Function cachingfrom itertools import product
+from functools import reduce, lru_cache
+import uuid
+
+
+
+# --------------------------
+# Order ID Generator
+# --------------------------
+
+def generate_order_id():
+
+    return "ORD-" + str(uuid.uuid4())[:8]
+
+
+
+# --------------------------
+# Product Combination
+# --------------------------
+
+def create_variations(colors, sizes):
+
+    variations = []
+
+    for color, size in product(colors, sizes):
+
+        variations.append(
+            {
+                "color": color,
+                "size": size
+            }
+        )
+
+    return variations
+
+
+
+# --------------------------
+# Cart Total
+# --------------------------
+
+def calculate_total(prices):
+
+    return reduce(
+        lambda x,y: x+y,
+        prices
+    )
+
+
+
+# --------------------------
+# Function Caching
+# --------------------------
+
+@lru_cache(maxsize=100)
+def product_price(product_id):
+
+    print(
+        "Database Query Running..."
+    )
+
+    prices = {
+        1:500,
+        2:1000,
+        3:1500
+    }
+
+    return prices.get(
+        product_id
+    )
+
+
+
+# --------------------------
+# Testing
+# --------------------------
+
+
+order_id = generate_order_id()
+
+print(
+    "Order ID:",
+    order_id
+)
+
+
+colors = [
+    "Black",
+    "White"
+]
+
+sizes = [
+    "M",
+    "L"
+]
+
+
+print(
+    "\nProduct Variations:"
+)
+
+print(
+    create_variations(
+        colors,
+        sizes
+    )
+)
+
+
+
+cart = [
+    500,
+    1000,
+    2000
+]
+
+
+print(
+    "\nCart Total:",
+    calculate_total(cart)
+)
+
+
+
+print(
+    "\nFirst Call:"
+)
+
+print(
+    product_price(1)
+)
+
+
+print(
+    "\nSecond Call:"
+)
+
+print(
+    product_price(1)
+)
--- Standard_Library/itertool&functools_module/itertool_module.py
+++ Standard_Library/itertool&functools_module/itertool_module.py
@@ -1,1 +1,303 @@
 
+
+
+### 1. itertools কী?
+"""
+itertools হলো Python-এর built-in module, যেখানে অনেক iterator building tools আছে।
+
+সহজ ভাষায়:
+
+বড় data efficiently process করার জন্য itertools ব্যবহার করি।
+
+এটা memory efficient কারণ অনেক ক্ষেত্রে এটি একসাথে সব data তৈরি করে না, প্রয়োজন অনুযায়ী দেয়।
+
+"""
+# import 
+import itertools
+
+
+
+### 1. count()
+# count কী?
+# Count একটি infinite counter তৈরি করে।
+
+from itertools import count
+
+counter = count()
+
+print(next(counter))
+print(next(counter))
+print(next(counter))
+print(next(counter))
+print(next(counter))
+print(next(counter))
+
+# starting value
+counter = count(10)
+
+print(next(counter))
+print(next(counter))
+print(next(counter))
+
+
+# Order ID generate করতে হবে:
+from itertools import count
+
+order_id = count(1000)
+
+print(next(order_id))
+print(next(order_id))
+
+
+
+
+
+### 2. cycle()
+# cycle কী?
+# কোনো iterable বারবার repeat করে।
+from itertools import cycle
+
+colors = cycle(
+   ["Red", "Green", "Blue"]
+)
+
+print(next(colors))
+print(next(colors))
+print(next(colors))
+print(next(colors))
+print(next(colors))
+
+
+# Backend Example: Round Robin System
+"""
+Round Robin হলো একটি load balancing algorithm 
+যেখানে incoming request গুলো একটার পর একটা করে 
+বিভিন্ন server-এর মধ্যে ভাগ করে দেওয়া হয়।
+
+সহজ ভাষায়:
+একাধিক server থাকলে সব request যেন একটি server-এর উপর চাপ না ফেলে, 
+এজন্য request গুলো পালাক্রমে (turn by turn) distribute করা হয়।
+"""
+# simple round robin system
+from itertools import cycle
+
+servers = [
+   "server-1",
+   "server-2",
+   "server-3"
+]
+
+server_cycle = cycle(servers)
+
+requests = [
+   "request-1",
+   "request-2",
+   "request-3",
+   "request-4",
+   "request-5",
+   "request-6",
+   "request-7",
+   "request-8"
+]
+
+for requset in requests:
+   server = next(server_cycle)
+
+   print(
+      requset,
+      "➡️ ",
+      server
+   )
+
+# Real Mini Load Balancer Class
+from itertools import cycle
+
+
+class LoadBalancer:
+
+    def __init__(self, servers):
+
+        self.servers = cycle(servers)
+
+
+    def get_server(self):
+
+        return next(self.servers)
+
+
+
+servers = [
+    "Django-Server-1",
+    "Django-Server-2",
+    "Django-Server-3"
+]
+
+
+lb = LoadBalancer(servers)
+
+
+for i in range(10):
+
+    server = lb.get_server()
+
+    print(
+        f"Request {i+1} handled by {server}"
+    )
+
+
+
+
+### 3. repeat()
+# repeat কী?
+# একই value বারবার দেয়।
+from itertools import repeat
+
+for item in repeat("Mamun is a profession Software Engineer.", 5):
+   print(item)
+
+for item in repeat("USER", 5):
+   print(item)
+
+
+
+
+
+### 4. chain()
+# chain কী?
+# একাধিক iterable একসাথে যুক্ত করে।
+
+
+from itertools import chain
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+
+result = chain(a, b)
+print(list(result))
+
+# active users
+from itertools import chain
+active_users = [
+   "Mamun",
+   "Rahim"
+]
+
+inactive_users = [
+   "Karim"
+]
+
+all_users = chain(
+   active_users,
+   inactive_users
+)
+print(list(all_users))
+
+
+
+
+
+### 5. combinations()
+# combinations কী?
+# Order matter করে না।
+# Example 1:
+# 3 জন থেকে 2 জন নির্বাচন:
+from itertools import combinations
+
+team = [
+   "A",
+   "B",
+   "C"
+]
+
+result = combinations(
+   team,
+   2
+)
+
+print(list(result))
+
+# example 2:
+from itertools import combinations
+product = [
+   "Laptop",
+   "Mouse",
+   "Bag"
+]
+
+product_bundle = combinations(
+   product,
+   2
+)
+
+print(list(product_bundle))
+
+
+
+
+
+### 6. Permutations()
+# permutations কী?
+# Order matter করে।
+from itertools import permutations
+
+items = [
+   "A",
+   "B",
+   "C"
+]
+
+result = permutations(
+   items,
+   2
+)
+
+print(list(result))
+
+
+
+
+### 7. product()
+# product কী?
+# সব possible combination তৈরি করে।
+from itertools import product
+
+colors = [
+   "Red",
+   "Blue"
+]
+
+sizes = [
+   "M",
+   "L"
+]
+
+result = product(
+   colors,
+   sizes
+)
+
+print(list(result))
+
+
+
+
+### 8. accumulate()
+# accumulate কী?
+# Running calculation করে।
+from itertools import accumulate
+
+numbers = [
+   1, 2, 3, 4
+]
+
+result = accumulate(numbers)
+print(list(result))
+
+##
+sales = [
+   100,
+   200,
+   300,
+   400
+]
+print(list(accumulate(sales)))
